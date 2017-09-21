18fa72b2f136473ac7bf4199ad0c83bee8e9f7d0
ThomasTetz <thomas.tetz@gmail.com>
2017-09-21 14:08:01 -0600

    Lab 3 Assignment
    Clears tweets on screen
    Removes tweets from disk

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -41,6 +41,7 @@ public class LonelyTwitterActivity extends Activity {
 
 		bodyText = (EditText) findViewById(R.id.body);
 		Button saveButton = (Button) findViewById(R.id.save);
+		Button clearButton = (Button) findViewById(R.id.clear);
 		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
 
 		saveButton.setOnClickListener(new View.OnClickListener() {
@@ -81,6 +82,18 @@ public class LonelyTwitterActivity extends Activity {
 
             }
 		});
+
+		clearButton.setOnClickListener(new View.OnClickListener() {
+			  public void onClick(View v) {
+				  setResult(RESULT_OK);
+
+                  tweets.clear();
+                  adapter.notifyDataSetChanged();
+                  clearFile();
+              }
+		  }
+
+		);
 	}
 
 	@Override
@@ -148,4 +161,9 @@ public class LonelyTwitterActivity extends Activity {
             throw new RuntimeException();
 		}
 	}
+
+	private void clearFile(){
+            deleteFile(FILENAME);
+    }
+
 }
