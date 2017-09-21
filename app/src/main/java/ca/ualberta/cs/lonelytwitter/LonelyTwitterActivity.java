413dcb1f1da697724485cfb1e8fe60ddb1d04878
ThomasTetz <thomas.tetz@gmail.com>
2017-09-20 19:02:43 -0600

    Lab 3 TA led changes
    notifyDataSetChanged
    Gson / Json
    data saved as ArrayList<Tweet>

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -6,6 +6,8 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -20,12 +22,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 public class LonelyTwitterActivity extends Activity {
 
-	private static final String FILENAME = "file2.sav";
+	private static final String FILENAME = "file.sav";
 	private EditText bodyText;
 	private ListView oldTweetsList;
-	
+
+	private ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+	private ArrayAdapter<Tweet> adapter;
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -42,15 +49,18 @@ public class LonelyTwitterActivity extends Activity {
 				setResult(RESULT_OK);
 				String text = bodyText.getText().toString();
 
-				List moods = new ArrayList();
-				Mood happyMood = new HappyMood();
-				Mood sadMood = new SadMood();
-				moods.add(happyMood.getMoodString());
+                tweets.add(new NormalTweet(text));
+                adapter.notifyDataSetChanged();
+                saveInFile();
+
+//				List moods = new ArrayList(););
 //				moods.add(sadMood.getMoodString());
 
-				saveInFile(text, new Date(System.currentTimeMillis()), moods);
+//				saveInFile(text, new Date(System.currentTimeMillis()), moods);
+//                saveInFile(text, new Date(System.currentTimeMillis()));
+//                saveInFile(text, new Date(System.currentTimeMillis()));
 
-                finish();
+//                finish();
 
 //              In-lab exercises
 ////				Tweet tweet = new Tweet(""); // abstract, cannot be instantiated
@@ -77,46 +87,65 @@ public class LonelyTwitterActivity extends Activity {
 	protected void onStart() {
 		// TODO Auto-generated method stub
 		super.onStart();
-		String[] tweets = loadFromFile();
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				R.layout.list_item, tweets);
+//		String[] tweets = loadFromFile();
+        loadFromFile();
+		adapter = new ArrayAdapter<Tweet>(this, R.layout.list_item, tweets);
 		oldTweetsList.setAdapter(adapter);
 	}
 
-	private String[] loadFromFile() {
-		ArrayList<String> tweets = new ArrayList<String>();
+//	private String[] loadFromFile() {
+    private void loadFromFile() {
+//		ArrayList<String> tweets = new ArrayList<String>();
 		try {
 			FileInputStream fis = openFileInput(FILENAME);
 			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-			String line = in.readLine();
-			while (line != null) {
-				tweets.add(line);
-				line = in.readLine();
-			}
+
+            // add dependency: File > Project Structure > app < Dependencies < + < dependency
+            Gson gson = new Gson();
+            Type listType = new TypeToken<ArrayList<NormalTweet>>() {}.getType();
+            tweets = gson.fromJson(in, listType);
+
+//            String line = in.readLine();
+//			while (line != null) {
+//				tweets.add(line);
+//				line = in.readLine();
+//			}
 
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+//			e.printStackTrace();
+            tweets = new ArrayList<Tweet>();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+//			e.printStackTrace();
+            throw new RuntimeException(e);
 		}
-		return tweets.toArray(new String[tweets.size()]);
+//		return tweets.toArray(new String[tweets.size()]);
 	}
-	
-	private void saveInFile(String text, Date date, List moods) {
+
+//    private void saveInFile(String text, Date date, List moods) {
+//    private void saveInFile(String text, Date date) {
+    private void saveInFile() {
 		try {
-			FileOutputStream fos = openFileOutput(FILENAME,
-					Context.MODE_APPEND);
-			fos.write(new String(date.toString() + " | " + moods + " " + text+"\n")
-					.getBytes());
+//			FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_APPEND);
+//
+            FileOutputStream fos = openFileOutput(FILENAME,
+                    Context.MODE_PRIVATE);
+            OutputStreamWriter writer = new OutputStreamWriter(fos);
+            Gson gson = new Gson();
+            gson.toJson(tweets, writer);
+            writer.flush();
+// fos.write(new String(date.toString() + " | " + text+"\n")
+//					.getBytes());
 			fos.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+//			e.printStackTrace();
+            throw new RuntimeException();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+//			e.printStackTrace();
+            throw new RuntimeException();
 		}
 	}
 }
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -55,5 +55,10 @@ public abstract class Tweet implements Tweetable{
 
     public abstract Boolean isImportant();
 
+    @Override
+    public String toString(){
+        return date.toString() + " | " + message;
+    }
+
 }
 
